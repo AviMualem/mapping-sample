e1be98a2338a2c0780cf50590f5f8ec4f82a0452
Avi Mualem <avimu@wix.com>
2020-01-26 19:41:25 +0200

    import is nice

--- src/main/scala/mapping/sample/SampleMappingApp.scala
+++ src/main/scala/mapping/sample/SampleMappingApp.scala
@@ -1,5 +1,4 @@
 package mapping.sample
-
 import io.scalaland.chimney.dsl._
 //import TransformerInstances._
 
@@ -7,33 +6,38 @@ import io.scalaland.chimney.dsl._
 case class RootSourceObject(sourceName: String, sourceNestedItemList: Seq[SourceNestedItem])
 
 case class SourceNestedItem(sourceNestedItemName: String, price: BigDecimal,
-                            sourceSecondLevelNestedList: Seq[SourceSecondLevelNestedObject])
+                            sourceSecondLevelNestedList:Seq[SourceSecondLevelNestedObject])
 
-case class SourceSecondLevelNestedObject(sourceSecondLevelNestedItemName: String)
+case class SourceSecondLevelNestedObject(sourceSecondLevelNestedItemName:String)
 
 case class RootTargetObject(targetName: String, targetNestedItemList: Seq[TargetNestedItem])
 
-case class TargetNestedItem(targetNestedItemName: String, price: BigDecimal, targetSecondLevelNestedList: Seq[TargetSecondLevelNestedObject])
+case class TargetNestedItem(targetNestedItemName: String, price: BigDecimal, targetSecondLevelNestedList:Seq[TargetSecondLevelNestedObject])
 
-case class TargetSecondLevelNestedObject(targetSecondLevelNestedItemName: String)
+case class TargetSecondLevelNestedObject(targetSecondLevelNestedItemName:String)
+
+
 
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
+    import Mappers._
 
-    val sourceObjectInstance = RootSourceObject("src-name", List(SourceNestedItem("nested-name", 22,
+
+
+    val sourceObjectInstance = RootSourceObject("src-name",List(SourceNestedItem("nested-name",22,
       List(SourceSecondLevelNestedObject("2name")))))
 
     /**
       * i would expect this will work due the the fact that all mapping definitions for root and nested object are provided via
       * implicit variables.
-      */
+       */
 
 
     val mappedResult = sourceObjectInstance.into[RootTargetObject].transform
 
-    val x = 2
+    val x=2
 
   }
 }
