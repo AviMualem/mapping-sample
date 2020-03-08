1e8544a0c02ce053117187dfd0af7f9b8b5ee709
Ittai Zeidman <ittaiz@wix.com>
2020-03-08 15:53:50 +0200

    remove commented out code and updated chimney version

--- src/main/scala/mapping/sample/Mappers.scala
+++ src/main/scala/mapping/sample/Mappers.scala
@@ -2,35 +2,6 @@ package mapping.sample
 
 import io.scalaland.chimney.Transformer
 import org.joda.time.{DateTime, DateTimeZone}
-//import mapping.sample.TransformerDefinitions.secondLevelNestedObjectTransformer
-
-//object TransformerDefinitions {
-//
-//  val secondLevelNestedObjectTransformer : Transformer[SourceSecondLevelNestedObject, TargetSecondLevelNestedObject]={
-//      _.into[TargetSecondLevelNestedObject]
-//      .withFieldRenamed(_.sourceSecondLevelNestedItemName,_.targetSecondLevelNestedItemName)
-//      .transform
-//  }
-//
-//  val nestedItemTransformer: Transformer[SourceNestedItem, TargetNestedItem] = {
-//
-//    implicit val v = secondLevelNestedObjectTransformer
-//    _.into[TargetNestedItem]
-//      .withFieldRenamed(_.sourceNestedItemName, _.targetNestedItemName)
-//      .withFieldRenamed(_.sourceSecondLevelNestedList,_.targetSecondLevelNestedList)
-//      .transform
-//  }
-//
-//
-//  val rootObjectTransformer: Transformer[RootSourceObject, RootTargetObject] = {
-//     implicit val justForImport = nestedItemTransformer
-//
-//    _.into[RootTargetObject]
-//      .withFieldRenamed(_.sourceName, _.targetName)
-//      .withFieldRenamed(_.sourceNestedItemList, _.targetNestedItemList)
-//      .transform
-//  }
-//}
 
 
 object Mappers {
